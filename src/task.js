5cd1d6aba041b960240754d492022ca905dc6b46
Eric Carlson <eric.carlson2972@gmail.com>
2018-08-23 14:25:36 -0400

    6-customize-the-appearance

--- src/task.js
+++ src/task.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
+import { isDragging } from 'react-beautiful-dnd';
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
 `;
 
 export default class Task extends React.Component {
   render() {
     return (
     <Draggable draggableId={this.props.task.id} index={this.props.index}>
-    {(provided) => (
+    {(provided, snapshot) => (
       <Container
         {...provided.draggableProps}
         {...provided.dragHandleProps}
         innerRef={provided.innerRef}
+        isDragging={snapshot.isDragging}
       >
         {this.props.task.content}
       </Container>
