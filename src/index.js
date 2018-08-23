a241058d7a770c68d33522d53049b21dd9d645c9
Eric Carlson <eric.carlson2972@gmail.com>
2018-08-23 11:35:12 -0400

    4-reorder-a-list

--- src/index.js
+++ src/index.js
@@ -8,16 +8,20 @@ import Column from './column';
 class App extends React.Component {
   state = initialData;
 
+  onDragEnd = result => {
+    // TO do - reorder the column
+  }
+
   render(){
     return (
-    //<DragDropContext>
-      this.state.columnOrder.map(columnId => {
+    <DragDropContext onDragEnd={this.onDragEnd}>
+      {this.state.columnOrder.map(columnId => {
       const column = this.state.columns[columnId];
       const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
       return <Column key={column.id} column={column} tasks={tasks} />;
-    }) // map()
-    //</DragDropContext>
+    })}
+    </DragDropContext>
   ); // return
   } // render
 } // class
